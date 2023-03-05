09dd42a0eab63a091ed8acada10893f6f710948b
Tan Le <tan.tran@siliconstack.com.au>
2023-03-06 00:58:29 +0700

    submit via client table

--- app/controller/thesis-defense-schedule.controller.ts
+++ app/controller/thesis-defense-schedule.controller.ts
@@ -1,60 +1,12 @@
-import { parse } from "csv-parse";
 import { Request, Response } from "express";
-import { readFileSync } from "fs";
-import { cloneDeep } from "lodash";
-import { StudentList } from "../interface/schedule.interface";
-import { Student } from "../interface/student.interface";
 
 export const thesisDefenseScheduleController = {
   studentList: {
     import: async (req: Request, res: Response) => {
-      const data = readFileSync(req.file?.path);
+      const { data } = req.body;
 
-      if (!data) return res.status(400);
-
-      parse(data, (e, r) => handleParseStudentList(res, e, r));
-      return res.status(200);
+      console.log(data);
+      return res.status(200).json({message: ""});
     },
   },
 };
-
-export function handleParseStudentList(res, err, records) {
-  if (err) {
-    return res
-      .status(400)
-      .json({ success: false, message: "An error occurred" });
-  }
-
-  console.log(handleConvertStudentList(records));
-
-  return res.json({ data: records });
-}
-
-export function handleConvertStudentList(list: any[][]): Student[] {
-  const listKeys = cloneDeep(list[0]);
-  list.shift();
-
-  const reducedStudentList: any[] = list.reduce((prevArr, curArr) => {
-    const mapped = listKeys.reduce((prevKey, curKey, index) => {
-      return { ...prevKey, [curKey]: curArr[index] };
-    }, {});
-
-    return [...prevArr, mapped];
-  }, []);
-
-  return reducedStudentList.reduce((prevStudent, curStudent): Student[] => {
-    return [
-      ...prevStudent,
-      {
-        MSSV: curStudent.MSSV,
-        class: curStudent.class,
-        department: curStudent.department,
-        major: curStudent.major,
-        email: curStudent.email,
-        firstName: curStudent.firstName,
-        lastName: curStudent.lastName,
-        thesisDefenseTime: null,
-      },
-    ];
-  }, []);
-}
